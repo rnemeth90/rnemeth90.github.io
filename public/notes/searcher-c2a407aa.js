749af8b8eae0d4762f56abfb76f7ebae53d5f5e9
ryan nemeth <ryanthomasnemeth@gmail.com>
2025-12-10 13:48:29 -0500

    notes

--- public/notes/searcher-c2a407aa.js
+++ public/notes/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2fd91b51.js',
+                path_to_root + 'searchindex-34c7ef29.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
