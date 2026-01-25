b29c6da71ce002cdf57d529b5f5f374c28ba298e
ryan nemeth <ryanthomasnemeth@gmail.com>
2026-01-24 22:47:04 -0500

    notes

--- public/notes/searcher-c2a407aa.js
+++ public/notes/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-45f66119.js',
+                path_to_root + 'searchindex-4f962408.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
