29aa7ad54e15e6a2464c208d39bd1ac837b1784d
ryan nemeth <ryan.nemeth@aprimo.com>
2026-07-06 14:16:04 -0400

    notes

--- public/notes/searcher-c2a407aa.js
+++ public/notes/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-52323166.js',
+                path_to_root + 'searchindex-adec9fac.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
