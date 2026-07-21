0bdbd631bd05ad6cbbd73a26b25138d87d9e6695
ryan nemeth <ryan.nemeth@aprimo.com>
2026-07-21 16:34:25 -0400

    update tenant-configuration to use new interpolation syntax

--- public/notes/searcher-c2a407aa.js
+++ public/notes/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-cbad76df.js',
+                path_to_root + 'searchindex-9f1c01d6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
