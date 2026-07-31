f6de3e67a3623ddbddf9301bc8f1eb4f28db1b91
rnemeth90 <rnemeth90@users.noreply.github.com>
2026-07-31 14:23:18 +0000

    deploy: ac0bcdc5f3a30c7d77f32b561790978e8fab1513

--- notes/searcher-c2a407aa.js
+++ notes/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-67dcea17.js',
+                path_to_root + 'searchindex-674b2f7c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
