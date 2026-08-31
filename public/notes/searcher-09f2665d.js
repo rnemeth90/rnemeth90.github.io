233699e4ff1a9aa44889df0064765135a2b11d1a
ryan <ryannemeth@live.com>
2026-08-30 22:06:50 -0400

    notes

--- public/notes/searcher-09f2665d.js
+++ public/notes/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-190f6cd0.js',
+                path_to_root + 'searchindex-e5200d07.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
